73e4e22663c2925d65d646d8ecefd7a1676e5b7e
Mubeen142 <mubeenullah323@gmail.com>
2023-07-03 21:57:22 +0200

    fix

--- src/Http/Controllers/SsoController.php
+++ src/Http/Controllers/SsoController.php
@@ -6,6 +6,7 @@ use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Encryption\Encrypter;
+use Pterodactyl\Models\User;
 
 class SsoController extends Controller
 {
@@ -25,18 +26,30 @@ class SsoController extends Controller
             if (strlen($secretKey) !== 32) {
                 return redirect('/login')->withErrors(['sso_error' => 'Secret key length must be 32 characters.']);
             }
+
             $cipher = config('app.cipher');
             $encrypter = new Encrypter($secretKey, $cipher);
             $encryptedToken = $request->input('token');
+
             try {
                 $decryptedToken = $encrypter->decrypt($encryptedToken);
             } catch (\Illuminate\Contracts\Encryption\DecryptException $e) {
                 return redirect('/login')->withErrors(['sso_error' => $e->getMessage()]);;
             }
+
             $authMarkerData = json_decode($decryptedToken, true);
             if (hash_equals($authMarkerData['secret_key'], $secretKey)) {
-                $user = \DB::table('users')->where('email', $authMarkerData['email'])->first();
+                $user = User::whereEmail($authMarkerData['email'])->firstOrFail();
                 if ($user) {
+
+                    if(!$user['root_admin']) {
+                        return redirect()->back()->withError('You cannot automatically login to admin accounts.');
+                    }
+
+                    if($user['2fa']) {
+                        return redirect()->back()->withError('Logging into accounts with 2 Factor Authentication enabled is not supported.');
+                    }
+                    
                     Auth::loginUsingId($user->id);
                     return redirect()->intended($route);
                 }
