bb226e40b460e65184cdd4390067e138ce63340a
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2023-07-09 02:15:09 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Gigabait\Sso\Http\Controllers\SsoController;
 
 Route::middleware(['web'])->group(function () {
-    Route::get('/sso-login', [SsoController::class, 'login'])->name('sso-login');
+    Route::get('/sso-wemx', [SsoController::class, 'webhook']);
+    Route::get('/sso-wemx/{token}', [SsoController::class, 'handle']);
 });
